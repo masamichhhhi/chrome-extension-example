767d22a73379777255b24cbdbb14503b8a2a7a22
masamichhhhi <masamichhhi@gmail.com>
2022-01-04 20:46:40 +0900

    add new tab

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => <div>App</div>;
+const App = () => {
+  return (
+    <div
+      style={{ backgroundColor: "blue", color: "white", textAlign: "center" }}
+    >
+      This is React!
+    </div>
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const app = document.createElement("div");
+document.body.prepend(app);
+ReactDOM.render(<App />, app);
